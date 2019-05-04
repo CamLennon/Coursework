d6d1421efdf8a0522aa7923e9234b099a2a9a530
Cam Lennon <48378706+CamLennon@users.noreply.github.com>
2019-05-04 20:44:45 +0100

    Update login.php

--- login.php
+++ login.php
@@ -6,11 +6,16 @@
   <link rel="stylesheet" type="text/css" href="styleDriving.css">
 </head>
 <body>
-  <form action="login.php" method="post">
-    Username:<input type="text" name="usnm"><br>
-    Password:<input type="password" name="pswd"><br>
-    <br>
-    <input type="submit" value="Log In">
-  </form>
+  <form action="loginprocess.php" method= "POST">
+  <?php
+  echo '<input type="hidden" name="location" value="';
+  if(isset($_GET['location'])) {
+      echo htmlspecialchars($_GET['location']);
+  }else{$_GET['Location']="index.php";}
+  echo '" />'
+  ?>
+    User name:<input class="form-group" type="text" name="Username"><br>
+    Password:<input class="form-group" type="password" name="Pword"><br>
+  <input type="submit" value="Login">
 </body>
 </html>
