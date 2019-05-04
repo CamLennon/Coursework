ca157681367557d618ade9511552fdcf0fc151b4
Cam Lennon <48378706+CamLennon@users.noreply.github.com>
2019-05-04 20:28:46 +0100

    Create users.php

---
+++ users.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="styleDriving.css">
+</head>
+<body>
+  <form action="addusers.php" method="post">
+    Full Name:<input type="text" name="name"><br>
+    Username:<input type="text" name="username"><br>
+    Password:<input type="password" name="password"><br>
+    Email:<input type="text" name="email"><br>
+    Contact Number:<input type="text" name="phone"><br>
+    1st Address:<input type="text" name="address1"><br>
+    2nd Address:<input type="text" name="address2"><br>
+    3rd Address:<input type="text" name="address3"><br>
+    <br>
+    <input type="submit" value="Add User">
+  </form>
+
+
+</body>
+</html>
